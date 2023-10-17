7fb4c3f6ce9cb4ea304e27b31ba6e86ac3497670
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-10-17 01:16:02 +0100

    Fix some bugs

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+class square():
+    """
+    A square class
+    Attributes:
+    width = 0
+    height = 0
+    """
+
+    def __init__(self, width=0, height=0):
+        '''Define the square initials'''
+        self.width = width
+        self.height = height
+
+    def area_of_my_square(self):
+        """ Define the Area of the square """
+        return self.width * self.width
+
+    def PermiterOfMySquare(self):
+        '''Define the perimeter of the square'''
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        '''Define the string of the square'''
+        return "{}/{}".format(self.width, self.height)
+
+    if __name__ == "__main__":
+        s = square(width=12, height=9)
+        print(s)
+        print(s.area_of_my_square())
+        print(s.PermiterOfMySquare())
